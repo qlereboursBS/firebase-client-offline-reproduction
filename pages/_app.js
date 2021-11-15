0761a004c9321c02ac75cbc50772cfea0104961c
qlerebours <quentin.lerebours@bearstudio.fr>
2021-11-15 11:14:59 +0100

    feat: Client offline error

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,29 @@
 import '../styles/globals.css'
+import { initializeApp, getApps } from 'firebase/app';
+import { useEffect } from "react";
+import { getDatabase, get, ref } from 'firebase/database'
+
+const FIREBASE_CONFIG = { }; // TODO replace with your config
+
+if (!getApps().length) {
+  initializeApp(FIREBASE_CONFIG);
+}
 
 function MyApp({ Component, pageProps }) {
+  const getData = async () => {
+    try {
+      const data = await get(ref(getDatabase(), '/')); // TODO replace with the data you want to fetch
+      console.log('Got data successfully', data)
+    } catch (e) {
+      console.error('An error occurred while getting data');
+      console.error(e);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return <Component {...pageProps} />
 }
 
